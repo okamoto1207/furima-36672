61bd3f893f53d3d1ab65f3026f130ba754ec52bd
okamoto1207 <appletoxicspark12.7@gmail.com>
2021-12-22 23:03:09 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,25 @@ class User < ApplicationRecord
   has_many :orders
 
   with_options presence: true do
+    # ニックネームがニックネームが必須かつ一意性であること
+    validates :nickname, uniqueness: true
+
+    # メールアドレスが必須でありかつ一意性であること
+    validates :email,    uniqueness: true
+
+    # パスワードを入力した際に、半角英数字（空文字NG）以外の場合は、メッセージを表示
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
     validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
-    validates :nickname, uniqueness: true
-    validates :email,    uniqueness: true
+    
+    # お名前(全角)は、全角（漢字・ひらがな・カタカナ）での入力が必須
     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    
+    # お名前カナ(全角)は、全角（カタカナ）での入力が必須
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+
+    # 生年月日が必須
     validates :birthday
   end
 end
