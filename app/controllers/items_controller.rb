e2895e6aeb32f6527bb0fd9e2624cc36c9a9d561
okamoto1207 <appletoxicspark12.7@gmail.com>
2022-01-09 22:07:58 +0900

    商品一覧表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,7 @@ class ItemsController < ApplicationController
   # before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    # @items = Item.all
+    @items = Item.all
   end
 
   def new
