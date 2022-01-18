6f30c1d421c0b989b9f4e6cb15a7bb68c2b40581
okamoto1207 <appletoxicspark12.7@gmail.com>
2022-01-18 22:50:37 +0900

    秘密鍵と公開鍵の環境変数化

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,7 +24,7 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = "sk_test_b0504cc786a282d2277350e3"
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,        
       card: order_params[:token], 
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_3a6038bc9a00048fe6f84f1d");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
